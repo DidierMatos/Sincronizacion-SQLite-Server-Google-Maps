98b08b4959eda74aa2ad877206157c0fe42384bb
Didier Matos <d.matostorres@gmail.com>
2019-08-20 11:35:50 -0500

    Agregando boton flotante y avanzando en la configuracion de MVC

---
+++ app/src/main/java/com/mayandevelopers/pftp/controllers/RvArbolesController.java
@@ -0,0 +1,92 @@
+package com.mayandevelopers.pftp.controllers;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.mayandevelopers.pftp.R;
+import com.mayandevelopers.pftp.models.ArbolesModel;
+
+import java.util.ArrayList;
+
+public class RvArbolesController extends RecyclerView.Adapter<RvArbolesController.ViewHolder> {
+
+    private Context mContext;
+    private ArrayList<ArbolesModel> mData;
+    ArrayList<ArbolesModel> actividadItem;
+    private int selectPosition;
+
+    public RvArbolesController(Context mContext, ArrayList<ArbolesModel> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public RvArbolesController.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View view;
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        view = mInflater.inflate(R.layout.cardview_mis_arboles ,viewGroup,false);
+
+        return new RvArbolesController.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RvArbolesController.ViewHolder viewHolder, int position) {
+        final ArbolesModel misarboles = mData.get(position);
+
+        viewHolder.txtview_folio.setText(misarboles.getFolio());
+
+        viewHolder.btn_visitas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        viewHolder.btn_editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        viewHolder.btn_eliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtview_folio;
+        ImageButton btn_visitas, btn_editar, btn_eliminar;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtview_folio = itemView.findViewById(R.id.txtviewFolioCVarbol);
+            btn_visitas = itemView.findViewById(R.id.btnVistasCVarbol);
+            btn_editar = itemView.findViewById(R.id.btnEditarCVarbol);
+            btn_eliminar = itemView.findViewById(R.id.btnEliminarCVarbol);
+
+
+        }
+    }
+}
--- app/src/main/java/com/mayandevelopers/pftp/views/ArbolesActivity.java
+++ app/src/main/java/com/mayandevelopers/pftp/views/ArbolesActivity.java
@@ -1,16 +1,62 @@
 package com.mayandevelopers.pftp.views;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.mayandevelopers.pftp.R;
+import com.mayandevelopers.pftp.controllers.RvArbolesController;
+import com.mayandevelopers.pftp.models.ArbolesModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArbolesActivity extends AppCompatActivity {
+
+    ArrayList<ArbolesModel> arboles_model;
+    RecyclerView rv_mis_arboles;
+    RvArbolesController rv_arboles_controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arboles);
+
+        rv_mis_arboles = findViewById(R.id.rvMisArboles);
+
+
+        arboles_model = new ArrayList<>();
+
+
+        for (int i = 0; i< 5; i++){
+
+            //AGREGANDO LO NUEVO A LA LISTA  //
+            arboles_model.add(new ArbolesModel("1"));
+
+
+            //ASIGNANDO ADAPTADOR AL RECYCLER VIEW//
+            rv_arboles_controller = new RvArbolesController(ArbolesActivity.this, (ArrayList<ArbolesModel>) arboles_model);
+            rv_mis_arboles.setLayoutManager(new LinearLayoutManager(ArbolesActivity.this, LinearLayoutManager.VERTICAL,false ));
+            rv_mis_arboles.setAdapter(rv_arboles_controller);
+        }
+
+
+       /* Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
+        FloatingActionButton fab = findViewById(R.id.fabAddArbol);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            /*    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+            }
+        });
 
 
 
