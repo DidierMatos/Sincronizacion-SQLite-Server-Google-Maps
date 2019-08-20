5c6a47f22198cfe2df927c69ff845bcd34766303
Didier Matos <d.matostorres@gmail.com>
2019-08-20 09:40:54 -0500

    Boton iniciar sesion redireccionando a vista arboles (solo maquetado) y resolviendo bug de activado false

--- app/src/main/java/com/mayandevelopers/pftp/views/LoginActivity.java
+++ app/src/main/java/com/mayandevelopers/pftp/views/LoginActivity.java
@@ -1,16 +1,32 @@
 package com.mayandevelopers.pftp.views;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.mayandevelopers.pftp.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        btn_login = findViewById(R.id.btnEntrarLogin);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ArbolesActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
