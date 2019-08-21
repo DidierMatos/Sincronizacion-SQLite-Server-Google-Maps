8b9bc279d6cf117b94210a60b4907a8a4f8eb641
Didier Matos <d.matostorres@gmail.com>
2019-08-21 12:54:14 -0500

    Reutilizando misma interfaz xml para agregar y editar y tambien cambia el texto de acuerdo al texto

--- app/src/main/java/com/mayandevelopers/pftp/views/AgregarArbolActivity.java
+++ app/src/main/java/com/mayandevelopers/pftp/views/AgregarArbolActivity.java
@@ -1,7 +1,12 @@
 package com.mayandevelopers.pftp.views;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.mayandevelopers.pftp.R;
 
@@ -9,12 +14,36 @@ public class AgregarArbolActivity extends AppCompatActivity {
 
     String valorUpdate;
 
+    TextView txtview_add_arbol;
+    ImageButton imgbtn_ubicacion;
+    Button imgbtn_add_arbol;
+    TextInputEditText edtxt_especie, edtxt_centro, edtxt_folio, edtxt_latitud, edtxt_longitud;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_arbol);
 
+        txtview_add_arbol = findViewById(R.id.txtviewAddArbol);
+        edtxt_especie = findViewById(R.id.edtxtEspecie);
+        edtxt_centro = findViewById(R.id.edtxtCentro);
+        edtxt_folio = findViewById(R.id.edtxtLatitud);
+        edtxt_latitud = findViewById(R.id.edtxtFolio);
+        edtxt_longitud = findViewById(R.id.edtxtLongitud);
+        imgbtn_add_arbol = findViewById(R.id.btnAddArbol);
+        imgbtn_ubicacion = findViewById(R.id.imgbtnUbicacionAddArbol);
+
+
         valorUpdate = getIntent().getStringExtra("update");
+
+        if (valorUpdate == null){
+
+
+        }else{
+            imgbtn_add_arbol.setText("Actualizar");
+            txtview_add_arbol.setText("Actualizar Arbol");
+        }
 
     }
 }
